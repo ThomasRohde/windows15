5943936d3127f20739f8986cf58c092419e25a5b
Thomas Klok Rohde <thomas@rohde.name>
2025-12-14 09:01:48 +0100

    fix: update revision number for precacheAndRoute in service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-959db468'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.af09hvfjmmg"
+    "revision": "0.3lqgn740ktg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
